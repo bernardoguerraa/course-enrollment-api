38b56fbfbaa8872280c0367420778e79e0c14e75
bernardoguerraa <bernardocguerra9@gmail.com>
2026-04-01 10:16:17 -0300

    implementando swagger e finalizando organização

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const routes = require("./routes");
 const errorMiddleware = require("./middlewares/errorMiddleware");
-const routes = require("./routes"); 
+
+// Importações do Swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Rota da documentação (deve vir antes das rotas principais)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(routes);
-
-
 app.use(errorMiddleware);
-
 
 module.exports = app;
